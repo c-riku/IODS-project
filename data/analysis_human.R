4f1969fb93b1bbcf54904ec02362ceac07794522
Chris Amos Mattos <christine.ribeiro@helsinki.fi>
2018-12-02 23:48:41 +0200

    add week 5 tasks

---
+++ data/analysis_human.R
@@ -0,0 +1,89 @@
+setwd("D:/New folder/OneDrive for Business 1/Git/IODS/IODS-project/data")
+human <- read.table(file = "human.txt",
+                    header = TRUE,
+                    dec = ".")
+ 
+# Library
+library(GGally)
+library(corrplot)
+library(FactoMineR)
+library(tidyr)
+# Overview of data (task 1)
+summary(human)
+str(human)
+
+ggpairs(human)
+cor(human)%>%corrplot()
+
+# PCA on raw data (task 2)
+pca_human_raw <- prcomp(human, scale = FALSE)
+pca_human_raw
+
+# Among all variables, GNI had a stronger correlation with PC1. Since the variables weren't standardized,
+# the magnitude and range of some variables, especially of GNI, was much greater, so the PCA gave more emphasis to GNI.
+
+s <- summary(pca_human_raw)
+s
+# PC1 explains 100% of the total variance in the data.
+
+# draw a biplot of the principal component representation and the original variables
+biplot(pca_human_raw)
+
+# PCA with standardized data (task 3)
+
+pca_human_std <- prcomp(human, scale=TRUE)
+pca_human_std
+
+# Expec.birth, expect.edu, sec.edu, and GNI variables show a strong and negative correlation with PC1, while mort.ratio
+# and adolbirth showed a strong positive correlation with PC1. Repres.par and lab.force showed a strong positive 
+# correlation with PC2. We can say that PC1 represents well expec.edu
+
+# create and print out a summary of pca_human
+s <- summary(pca_human_std)
+s
+
+# assigning the proportion of variance as percent to each PC
+
+pca_pr <- round(100*s$importance[2, ], digits = 1)
+pc_lab<-paste0(names(pca_pr), " (", pca_pr, "%)")
+pc_lab
+
+#The PCA produced two components with eigenvalues (in units of standard deviation) above 1.0,
+#which explained about 70% of the total variance among the eight original variables.
+
+# draw a biplot of the principal component representation and the original variables
+biplot(pca_human_std, cex = c(0.8, 1), col = c("grey40", "deeppink2"), xlab = pc_lab[1], ylab = pc_lab[2])
+
+# Interpretation (task 4)
+# We can see that PC1 and PC1 explained 53.6% and 16.2% of the total variance, respectively. 
+# PC1 represents Expec.birth, expect.edu, sec.edu, and GNI, that strongly correlates with each other, which negatively
+# correlate with mort.ratio and adol.virth, that strongly correlate with each other. ON the other hand, PC2
+# represents repres.par and lab.force, which are independent from the other variables in the data set.
+# You can see they are orthogonally positioned on the biplot.
+
+# MCA
+
+data("tea")
+dim(tea)
+str(tea)
+
+keep_columns <- c("Tea", "age", "How", "where", "friends", "breakfast")
+tea_time <- select(tea, one_of(keep_columns))
+summary(tea_time)
+gather(tea_time) %>% ggplot(aes(value)) + facet_wrap("key", scales = "free") +geom_bar()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8))
+
+mca <- MCA(tea_time, quanti.sup= 2, graph = FALSE)
+summary(mca)
+# Of all variables,"where" correlated more strongly with dimension 1 (but also with dimension 3),
+# whereas "How" correlated more with dimension 2."Tea" type correlated with dimension 3.
+# All variables were significant in dim 1.
+# Chain store+tea shop was not significant for dim 2 nor for dim 3.
+# Tea with milk was not significant for dim 3.
+dimdesc(mca)
+# Age is significantly correlated with dim 2 (p<0.05) and dim 3 (p<0.001).
+
+plot(mca, invisible=c("ind"),habillage = "quali")
+
+
+
--- data/create_human.R
+++ data/create_human.R
@@ -23,8 +23,8 @@ colnames(hd)[3] <- "HDI"
 colnames(hd)[4] <- "expec.birth"
 colnames(hd)[5] <- "expec.edu"
 colnames(hd)[6] <- "mean.edu"
-colnames(hd)[7] <- "grossincome"
-colnames(hd)[8] <- "GNI"
+colnames(hd)[7] <- "GNI"
+colnames(hd)[8] <- "GNI.capita"
 
 colnames(gii)[3] <- "GII"
 colnames(gii)[4] <- "mort.ratio"
@@ -49,5 +49,88 @@ human <- inner_join(hd, gii, by = join_by)
 str(human)
 
 setwd("D:/New folder/OneDrive for Business 1/Git/IODS/IODS-project/data")
+write.table(human, file = "human.txt", sep = " ", dec=".", row.names = FALSE, col.names = TRUE)
+
+
+#Christine Ribeiro
+#1.12.2018
+
+setwd("D:/New folder/OneDrive for Business 1/Git/IODS/IODS-project/data")
+
+
+human <- read.table(file = "human.txt",
+                           header = TRUE,
+                           dec = ".")
+
+# Libraries
+library(dplyr)
+library(stringr)
+
+# Data exploration and description
+
+head(human)
+str(human)
+dim(human)
+names(human)
+summary(human)
+
+
+#Description
+#The dataset consists of several social-economical indicators from several countries.
+
+#data.frame':	195 obs. of  19 variables:
+#HDI.Rank   : Rank of the contry based on Human Development index (HDI)
+#Country    : Name of country
+#HDI        : Human Development index (the closer to 1 the better)
+#expec.birth: Life Expectancy at Birth
+#expec.edu  : Expected Years of Education 
+#mean.edu   : Mean Years of Education
+#GNI        : Gross National Income (GNI per Capita)
+#GNI.capita : GNI per Capita (Rank Minus HDI Rank)
+#GII.Rank   : Rank of the country based on Gender Inequality Index (GII)
+#GII        : Gender Inequality Index
+#mort.ratio : Maternal Mortality Ratio 
+#adol.birth : Adolescent Birth Rate
+#repres.par : Percent Representation in Parliament
+#sec.eduF   : Female Population with Secondary Education
+#sec.eduM   : Male Population with Secondary Education
+#labF       : Female Labour Force Participation Rate
+#labM       : Male Labour Force Participation Rate
+#sec.edu    : Ratio of Female and Male populations with secondary education 
+#lab.force  : Ratio of labour force participation of females and males
+
+# Remove comma from GNI and mutate it to numeric (task 1)
+
+human$GNI<- str_replace(human$GNI, pattern=",", replace ="")%>% as.numeric
+
+str(human$GNI)
+
+
+# Exclude unneeded variables (task 2)
+keep <- c("Country", "sec.edu", "lab.force", "expec.birth", "expec.edu",
+          "GNI", "mort.ratio", "adol.birth", "repres.par")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+
+
+# Remove all rows with missing values (task 3)
+
+colSums(is.na(human))
+human <- na.exclude(human)
+
+str(human)
+
+# Remove the observations which relate to regions instead of countries (task 4)
+
+tail(human, n=10)
+human <- human[1:155, ]
+tail(human, n=10)
+
+# Add countries as rownames then remove it (task 5)
+
+rownames(human) <- human$Country
+human <- select(human, -Country)
+
 write.table(human, file = "human.txt", sep = " ", dec=".", row.names = FALSE, col.names = TRUE)
 
